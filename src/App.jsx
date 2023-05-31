f281263e885aa2c45fa0e816cb4e52443a70eabd
sashasavin <praise1234@mail.ru>
2023-05-31 21:25:46 +0300

    add adaptive layout

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,8 @@ import { Portfolio, Main, Header } from "./pages"
 function App() {
   return (
     <div className="bg-gray-100 min-h-screen">
-      <div className="px-40 py-10">
-        <div className="mx-30 bg-white rounded-sm shadow-sm">
+      <div className="px-4 sm:px-8 md:px-16 lg:px-24 xl:px-32 py-6 sm:py-8 md:py-10">
+        <div className="mx-auto max-w-lg sm:max-w-xl md:max-w-2xl lg:max-w-4xl xl:max-w-5xl bg-white rounded-sm shadow-sm">
           <Header />
           <Main />
           <Portfolio />
--- src/pages/Header.jsx
+++ src/pages/Header.jsx
@@ -4,14 +4,14 @@ import { Link } from "react-router-dom"
 export const Header = () => {
   return (
     <header className="flex flex-col md:flex-row justify-between items-center text-black px-6 py-4">
-      <h1 className="text-4xl font-bold font-sans ">Савин А.</h1>
+      <h1 className="text-4xl font-bold font-sans mb-4 md:mb-0">Савин А.</h1>
       <div className="flex order-2">
         <button className="rounded-full bg-slate-100 mt-5 px-4 py-2 hover:bg-green-300">
           <Link to="https://t.me/funnymagnett">Свяжитесь со мной</Link>
         </button>
       </div>
 
-      <nav>
+      <nav className="flex mt-4 md:mt-0">
         <ul className="flex space-x-4">
           <li className="hover:opacity-50 cursor-pointer">
             <Link to="/instagram">
