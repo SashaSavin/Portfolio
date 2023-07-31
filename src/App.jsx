461b698d2de3d73a166a434da05c3e9409e29f07
sashasavin <praise1234@mail.ru>
2023-07-31 19:51:35 +0300

    fix layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
-import { Portfolio, Main, Header } from "./pages"
+import { Portfolio, Header } from './pages'
 
 function App() {
   return (
-    <div className="bg-gray-100 min-h-screen">
-      <div className="px-4 sm:px-8 md:px-16 lg:px-24 xl:px-32 py-6 sm:py-8 md:py-10">
-        <div className="mx-auto max-w-lg sm:max-w-xl md:max-w-2xl lg:max-w-4xl xl:max-w-5xl bg-white rounded-sm shadow-sm">
-          <Header />
-          <Main />
-          <Portfolio />
+    <div className='bg-gray-100 min-h-screen'>
+      <div className='px-4 sm:px-8 md:px-16 lg:px-24 xl:px-32 py-6 sm:py-8 md:py-10'>
+        <div className='mx-auto max-w-lg sm:max-w-xl md:max-w-2xl lg:max-w-4xl xl:max-w-5xl'>
+          <div className='mb-4 rounded-xl bg-white'>
+            <Header />
+          </div>
+
+          <div className='mb-4 rounded-xl bg-white'>
+            <Portfolio />
+          </div>
         </div>
       </div>
     </div>
--- src/pages/Header.jsx
+++ src/pages/Header.jsx
@@ -3,38 +3,46 @@ import { Link } from "react-router-dom"
 
 export const Header = () => {
   return (
-    <header className="flex flex-col md:flex-row justify-between items-center text-black px-6 py-4">
-      <h1 className="text-4xl font-bold font-sans mb-4 md:mb-0">Савин А.</h1>
-      <div className="flex order-2">
-        <button className="rounded-full bg-slate-100 mt-5 px-4 py-2 hover:bg-green-300">
-          <Link to="https://t.me/funnymagnett">Свяжитесь со мной</Link>
-        </button>
-      </div>
+    <>
+      <header className='flex flex-col md:flex-row justify-between items-center text-black px-6 py-4'>
+        <h1 className='text-4xl font-bold font-sans mb-4 md:mb-0'>Савин А.</h1>
+        <div className='flex order-2'>
+          <button className='rounded-full bg-slate-100 mt-5 px-4 py-2 hover:bg-green-300'>
+            <Link to='https://t.me/sanya_savin'>Свяжитесь со мной</Link>
+          </button>
+        </div>
 
-      <nav className="flex mt-4 md:mt-0">
-        <ul className="flex space-x-4">
-          <li className="hover:opacity-50 cursor-pointer">
-            <Link to="/instagram">
-              <FaInstagram />
-            </Link>
-          </li>
-          <li className="hover:opacity-50 cursor-pointer">
-            <Link to="/telegram">
-              <FaTelegram />
-            </Link>
-          </li>
-          <li className="hover:opacity-50 cursor-pointer">
-            <Link to="/vk">
-              <FaVk />
-            </Link>
-          </li>
-          <li className="hover:opacity-50 cursor-pointer">
-            <Link to="/whatsapp">
-              <FaWhatsapp />
-            </Link>
-          </li>
-        </ul>
-      </nav>
-    </header>
+        <nav className='flex mt-4 md:mt-0'>
+          <ul className='flex space-x-4'>
+            <li className='hover:opacity-50 cursor-pointer'>
+              <Link to='/instagram'>
+                <FaInstagram />
+              </Link>
+            </li>
+            <li className='hover:opacity-50 cursor-pointer'>
+              <Link to='/telegram'>
+                <FaTelegram />
+              </Link>
+            </li>
+            <li className='hover:opacity-50 cursor-pointer'>
+              <Link to='/vk'>
+                <FaVk />
+              </Link>
+            </li>
+            <li className='hover:opacity-50 cursor-pointer'>
+              <Link to='/whatsapp'>
+                <FaWhatsapp />
+              </Link>
+            </li>
+          </ul>
+        </nav>
+      </header>
+      
+      <section className='text-black px-6 py-8 md:max-w-2xl'>
+        <h2 className='text-5xl font-bold mb-4'>Александр Савин</h2>
+        <p className='text-lg font-medium mb-4'>Frontend-разработчик</p>
+        <p className='text-base mb-4'>Люблю красивые и понятные интерфейсы</p>
+      </section>
+    </>
   )
 }
